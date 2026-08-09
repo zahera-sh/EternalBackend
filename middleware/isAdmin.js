b10b452b8053683ecba3aa74f34187774d2086d0
zahera <zahera.shehab@protonmail.com>
2026-08-09 19:47:21 +0300

    middleware: create isAdmin

---
+++ middleware/isAdmin.js
@@ -0,0 +1,32 @@
+const User = require("../models/User");
+
+
+async function isAdmin(req, res, next) {
+
+    try {
+
+        const user = await User.findById(req.user.id);
+
+        if (!user) {
+            return res.status(404).json({
+                message: "User not found."
+            });
+        }
+
+        if (user.role !== "admin") {
+            return res.status(403).json({
+                message: "Unauthorized."
+            });
+        }
+
+        next();
+
+    } catch (err) {
+
+        res.status(500).json({ message: err.message });
+
+    }
+}
+
+
+module.exports = isAdmin;
